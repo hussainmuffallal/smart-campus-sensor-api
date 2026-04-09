9b66dae26a0149d15c6109a54cf93ed8f4229721
Hussain Muffallal <hussainmufallal2004@gmail.com>
2026-04-09 20:46:20 +0530

    feat(sensors): implement sensor registration and type-based filtering

---
+++ src/main/java/com/mycompany/smart/campus/api/resources/SensorResource.java
@@ -0,0 +1,72 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.smart.campus.api.resources;
+
+/**
+ *
+ * @author Hussain
+ */
+
+import com.mycompany.smart.campus.api.db.DataStore;
+import com.mycompany.smart.campus.api.models.Sensor;
+
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Path("/sensors")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class SensorResource {
+    
+    private final DataStore db = DataStore.getInstance();
+
+    // GET /api/v1/sensors?type={sensorType}
+    @GET
+    public Response getSensors(@QueryParam("type") String type) {
+        List<Sensor> resultList;
+
+        // If the user provided a '?type=' query, filter the list.
+        // Otherwise, return all sensors.
+        if (type != null && !type.trim().isEmpty()) {
+            resultList = db.getSensors().values().stream()
+                    .filter(sensor -> type.equalsIgnoreCase(sensor.getType()))
+                    .collect(Collectors.toList());
+        } else {
+            resultList = List.copyOf(db.getSensors().values());
+        }
+
+        return Response.ok(resultList).build();
+    }
+
+    // POST /api/v1/sensors
+    @POST
+    public Response registerSensor(Sensor sensor) {
+        // 1. Dependency Validation: Does the assigned room exist?
+        if (!db.getRooms().containsKey(sensor.getRoomId())) {
+            // NOTE: Returning a standard 400 for now. In Part 5, we upgrade 
+            // this to a custom Exception Mapper returning a 422!
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("{\"error\":\"Validation Failed: The specified roomId does not exist.\"}")
+                    .build();
+        }
+
+        // 2. Generate an ID if one isn't provided
+        if (sensor.getId() == null || sensor.getId().trim().isEmpty()) {
+            sensor.setId(UUID.randomUUID().toString());
+        }
+
+        // 3. Save the sensor
+        db.getSensors().put(sensor.getId(), sensor);
+        
+        // 4. Update the Room to know about this new sensor
+        db.getRooms().get(sensor.getRoomId()).getSensorIds().add(sensor.getId());
+
+        return Response.status(Response.Status.CREATED).entity(sensor).build();
+    }
+}
